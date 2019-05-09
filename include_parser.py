0670e8fcb1e07e722026066fa5df64af26c86138
Stanislas  Dolcini <stanislas.dolcini@avanade.com>
2019-05-09 10:18:15 +0200

    Pep8 Compliance
    Limit to 79 chars per line, 
    Replace tabs by spaces
    Add spaces after comas

--- include_parser.py
+++ include_parser.py
@@ -6,82 +6,92 @@ to_parse = []
 parsed = {}
 included_by = {}
 
+
 class IncludeParser(Preprocessor):
-	def __init__(self, file_to_parse, include_dirs, directly_includes, included_directly_by, includes, included_by):
-		# For internal consistency, we throw if the file to be parsed doesn't exist.
-		if not Path(file_to_parse).is_file():
-			print("error")
-		super().__init__()
+    def __init__(self,
+                 file_to_parse,
+                 include_dirs,
+                 directly_includes,
+                 included_directly_by,
+                 includes,
+                 included_by):
+        # For internal consistency, we throw if the file to be parsed doesn't
+        # exist.
+        if not Path(file_to_parse).is_file():
+            print("error")
+        super().__init__()
 
-		self.file_path = Path(file_to_parse).as_posix()
+        self.file_path = Path(file_to_parse).as_posix()
 
-		self.includes = includes
-		self.directly_includes = directly_includes
-		self.included_directly_by = included_directly_by
-		self.included_by = included_by
+        self.includes = includes
+        self.directly_includes = directly_includes
+        self.included_directly_by = included_directly_by
+        self.included_by = included_by
 
-		self.stop_at = set()
-		self.file_to_parse = file_to_parse
-		[self.add_path(path) for path in include_dirs]
+        self.stop_at = set()
+        self.file_to_parse = file_to_parse
+        [self.add_path(path) for path in include_dirs]
 
-	def add_stop_at(self, stop_at):
-		self.stop_at = self.stop_at.union(set(stop_at))
-		return self
+    def add_stop_at(self, stop_at):
+        self.stop_at = self.stop_at.union(set(stop_at))
+        return self
 
-	def pretend_being(self, compiler):
-		if compiler not in ['clang','gcc']:
-			print(f"Unknown compiler {compiler}")
-			return
-		import subprocess
-		defines = subprocess.run([compiler, '-x', 'c', '/dev/null', '-dM', '-E'], capture_output=True)
-		defines = defines.stdout.decode("utf-8").split('\n')
-		for define in defines:
-			if not define.startswith('#define'):
-				continue
-			self.define(define.replace('#define ',''))
-		return self
+    def pretend_being(self, compiler):
+        if compiler not in ['clang', 'gcc']:
+            print(f"Unknown compiler {compiler}")
+            return
+        import subprocess
+        defines = subprocess.run(
+            [compiler, '-x', 'c', '/dev/null', '-dM', '-E'],
+            capture_output=True)
+        defines = defines.stdout.decode("utf-8").split('\n')
+        for define in defines:
+            if not define.startswith('#define'):
+                continue
+            self.define(define.replace('#define ', ''))
+        return self
 
-	# Hook into parsegen to detect valide includes
-	def parsegen(self, input, source=None, abssource=None):
-		if not abssource:
-			return super().parsegen(input, source, abssource)
+    # Hook into parsegen to detect valide includes
+    def parsegen(self, input, source=None, abssource=None):
+        if not abssource:
+            return super().parsegen(input, source, abssource)
 
-		if os.path.relpath(abssource) in self.stop_at:
-			return []
+        if os.path.relpath(abssource) in self.stop_at:
+            return []
 
-		if hasattr(self, 'source'):
-			self.curr_file = os.path.relpath(self.source)
-		self.handle_header(os.path.relpath(abssource))
-		return super().parsegen(input, source, abssource)
+        if hasattr(self, 'source'):
+            self.curr_file = os.path.relpath(self.source)
+        self.handle_header(os.path.relpath(abssource))
+        return super().parsegen(input, source, abssource)
 
-	def on_include_not_found(self, is_system_include, curdir, includepath):
-		path = '<' + includepath + '>' if is_system_include else includepath
-		if path in self.stop_at:
-			return
-		self.handle_header(path)
+    def on_include_not_found(self, is_system_include, curdir, includepath):
+        path = '<' + includepath + '>' if is_system_include else includepath
+        if path in self.stop_at:
+            return
+        self.handle_header(path)
 
-	def handle_header(self, header):
-		if header == self.file_path:
-			return
+    def handle_header(self, header):
+        if header == self.file_path:
+            return
 
-		if self.curr_file not in self.directly_includes:
-			self.directly_includes[self.curr_file] = set()
-		self.directly_includes[self.curr_file].add(header)
+        if self.curr_file not in self.directly_includes:
+            self.directly_includes[self.curr_file] = set()
+        self.directly_includes[self.curr_file].add(header)
 
-		if header not in self.included_directly_by:
-			self.included_directly_by[header] = set()
-		self.included_directly_by[header].add(self.curr_file)
+        if header not in self.included_directly_by:
+            self.included_directly_by[header] = set()
+        self.included_directly_by[header].add(self.curr_file)
 
-		if self.file_path not in self.includes:
-			self.includes[self.file_path] = set()
-		self.includes[self.file_path].add(header)
+        if self.file_path not in self.includes:
+            self.includes[self.file_path] = set()
+        self.includes[self.file_path].add(header)
 
-		if header not in self.included_by:
-			self.included_by[header] = set()
-		self.included_by[header].add(self.file_path)
+        if header not in self.included_by:
+            self.included_by[header] = set()
+        self.included_by[header].add(self.file_path)
 
-	def run(self):
-		self.curr_file = self.file_to_parse
-		self.parse(open(self.file_to_parse,'r'))
-		while self.token():
-			pass
+    def run(self):
+        self.curr_file = self.file_to_parse
+        self.parse(open(self.file_to_parse, 'r'))
+        while self.token():
+            pass
--- precompiled_helper.py
+++ precompiled_helper.py
@@ -14,24 +14,33 @@ stop_at_headers = []
 
 prune_solo = False
 
+
 def unlist(lists):
-	from itertools import chain as unlist
-	return list(unlist.from_iterable(lists))
+    from itertools import chain as unlist
+    return list(unlist.from_iterable(lists))
+
 
 def sanitize_glob(regex):
-	return ".*?".join([st.replace('*', '[^/]+?') for st in	 re.split(r"\*\*/", regex)])
+    return ".*?".join([st.replace('*', '[^/]+?')
+                       for st in re.split(r"\*\*/", regex)])
 
-def exclude (file, excludes):
-	return any([re.search(sanitize_glob(exclude), file) is not None for exclude in excludes])
+
+def exclude(file, excludes):
+    return any([re.search(sanitize_glob(exclude), file)
+                is not None for exclude in excludes])
+
 
 def fetch_cpp_files(folder, excludes):
-	path = folder if re.search('.(cpp|h|hpp)$',folder) is not None else folder + "/**/*.cpp"
-	files = glob.glob(path, recursive=True)
-	return [file for file in files if not exclude(file, excludes)]
+    path = folder if re.search(
+        '.(cpp|h|hpp)$', folder) is not None else folder + "/**/*.cpp"
+    files = glob.glob(path, recursive=True)
+    return [file for file in files if not exclude(file, excludes)]
+
 
 def fetch_all_cpp_files(folders, excludes):
-	files = [fetch_cpp_files(folder, excludes) for folder in folders]
-	return unlist(files)
+    files = [fetch_cpp_files(folder, excludes) for folder in folders]
+    return unlist(files)
+
 
 directly_includes = {}
 included_directly_by = {}
@@ -39,85 +48,98 @@ includes = {}
 included_by = {}
 wd = os.getcwd()
 
+
 def pretend(parser):
-	parser.pretend_being('clang')
-	parser.define('MOZJS_MAJOR_VERSION 38')
-	parser.define('MOZJS_MINOR_VERSION 3')
-	parser.define('CONFIG_ENABLE_PCH 1')
-	parser.define('USING_PCH 1')
-	parser.define('HAVE_PCH 1')
-	parser.define('BOOST_VERSION 106000')
-	parser.define('UNICODE')
-	parser.define('SDL_VERSION_ATLEAST(a,b,c) 1')
-	return parser
+    parser.pretend_being('clang')
+    parser.define('MOZJS_MAJOR_VERSION 38')
+    parser.define('MOZJS_MINOR_VERSION 3')
+    parser.define('CONFIG_ENABLE_PCH 1')
+    parser.define('USING_PCH 1')
+    parser.define('HAVE_PCH 1')
+    parser.define('BOOST_VERSION 106000')
+    parser.define('UNICODE')
+    parser.define('SDL_VERSION_ATLEAST(a,b,c) 1')
+    return parser
+
 
 def parse_args():
-	global working_dir
-	global input_folders
-	global exclusions
-	global include_dirs
-	global precompiled
-	global remove_headers_from_precompiled
-	global stop_at_headers
+    global working_dir
+    global input_folders
+    global exclusions
+    global include_dirs
+    global precompiled
+    global remove_headers_from_precompiled
+    global stop_at_headers
 
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--file", help="Load settings from a config file")
-	args = parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", help="Load settings from a config file")
+    args = parser.parse_args()
 
-	if args.file:
-		import json
-		config = json.load(open(args.file, 'r'))
-		working_dir = config["working_dir"]
-		input_folders = config["input_folders"]
-		exclusions = config["exclusions"]
-		include_dirs = config["include_dirs"]
-		precompiled = config["precompiled"]
-		remove_headers_from_precompiled = config["remove_headers_from_precompiled"]
-		stop_at_headers = config["stop_at_headers"]
+    if args.file:
+        import json
+        config = json.load(open(args.file, 'r'))
+        working_dir = config["working_dir"]
+        input_folders = config["input_folders"]
+        exclusions = config["exclusions"]
+        include_dirs = config["include_dirs"]
+        precompiled = config["precompiled"]
+        remove_headers_from_precompiled =
+        config["remove_headers_from_precompiled"]
+        stop_at_headers = config["stop_at_headers"]
 
-	if precompiled:
-		include_dirs.append(os.path.relpath(Path(precompiled).parent))
-		stop_at_headers.append(precompiled)
+    if precompiled:
+        include_dirs.append(os.path.relpath(Path(precompiled).parent))
+        stop_at_headers.append(precompiled)
 
-	if working_dir:
-		os.chdir(working_dir)
+    if working_dir:
+        os.chdir(working_dir)
 
-	if remove_headers_from_precompiled and precompiled:
-		directly_includes = {}
-		included_directly_by = {}
-		includes = {}
-		included_by = {}
-		parser = IncludeParser(precompiled, include_dirs, directly_includes, included_directly_by, includes, included_by)
-		pretend(parser).run()
-		stop_at_headers = stop_at_headers + list(included_by.keys())
+    if remove_headers_from_precompiled and precompiled:
+        directly_includes = {}
+        included_directly_by = {}
+        includes = {}
+        included_by = {}
+        parser = IncludeParser(precompiled, include_dirs, directly_includes,
+                               included_directly_by, includes, included_by)
+        pretend(parser).run()
+        stop_at_headers = stop_at_headers + list(included_by.keys())
+
 
 if __name__ == "__main__":
-	parse_args()
+    parse_args()
 
-	files = fetch_all_cpp_files(input_folders, exclusions)
+    files = fetch_all_cpp_files(input_folders, exclusions)
 
-	def run(file):
-		parser = IncludeParser(file, include_dirs, directly_includes, included_directly_by, includes, included_by)
-		parser.add_stop_at(stop_at_headers)
-		pretend(parser).run()
-		print(f"Ran {file}")
+    def run(file):
+        parser = IncludeParser(file, include_dirs, directly_includes,
+                               included_directly_by, includes, included_by)
+        parser.add_stop_at(stop_at_headers)
+        pretend(parser).run()
+        print(f"Ran {file}")
 
-	[run(file) for file in files]
+    [run(file) for file in files]
 
-	os.chdir(wd)
+    os.chdir(wd)
 
-	with open('scores.csv','w') as out:
-		out.write("header;n;ndir;prop;explo;leaf\n")
-		for f in included_by:
-			if prune_solo and f in included_directly_by and len(included_directly_by[f]) ==1:
-				continue
-			out.write(f'{f};{len(included_by[f])};{len(included_directly_by[f])};{len(included_by[f])/len(files)};{(len(directly_includes[f]) if f in directly_includes else 0)*len(included_by[f])};{0 if f in directly_includes else 1}\n')
+    with open('scores.csv', 'w') as out:
+        out.write("header;n;ndir;prop;explo;leaf\n")
+        for f in included_by:
+            if prune_solo and f in included_directly_by and
+            len(included_directly_by[f]) == 1:
+                continue
+            out.write(
+                f'{f};{len(included_by[f])}; \
+                {len(included_directly_by[f])}; \
+                {len(included_by[f])/len(files)}; \
+                {(len(directly_includes[f])  \
+                if f in directly_includes else 0)*len(included_by[f])}; \
+                {0 if f in directly_includes else 1}\n')
 
-	with open('direct_includes.txt','w') as out:
-		for f in directly_includes:
-			out.write(f'{f} : {directly_includes[f]}\n')
+    with open('direct_includes.txt', 'w') as out:
+        for f in directly_includes:
+            out.write(f'{f} : {directly_includes[f]}\n')
 
-	with open('direct_included_by.txt','w') as out:
-		for f in included_directly_by:
-			out.write(f'{f} : {included_directly_by[f]}\n')
+    with open('direct_included_by.txt', 'w') as out:
+        for f in included_directly_by:
+            out.write(f'{f} : {included_directly_by[f]}\n')
